95bf70c10c62a0c909e60219b2017059e06bea40
LUISVILL02 <lvillamil@unimagdalena.edu.co>
2025-01-22 19:59:30 -0500

    Added: icons

---
+++ src/components/icons/skills/secondary/Figma.tsx
@@ -0,0 +1,16 @@
+
+import { CSSProperties } from "react"
+
+export const Figma = (props: { style: CSSProperties | undefined }) => {
+  return (
+    <svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg" style={props.style}>
+    <path d="M23 69C29.348 69 34.5 63.848 34.5 57.5V46H23C16.652 46 11.5 51.152 11.5 57.5C11.5 63.848 16.652 69 23 69Z" fill="#D9D9D9"/>
+    <path d="M11.5 34.5C11.5 28.152 16.652 23 23 23H34.5V46H23C16.652 46 11.5 40.848 11.5 34.5Z" fill="#B8B8B8"/>
+    <path d="M11.5 11.5C11.5 5.152 16.652 0 23 0H34.5V23H23C16.652 23 11.5 17.848 11.5 11.5Z" fill="white"/>
+    <path d="M34.5 0H46C52.348 0 57.5 5.152 57.5 11.5C57.5 17.848 52.348 23 46 23H34.5V0Z" fill="#B1B1B1"/>
+    <path d="M57.5 34.5C57.5 40.848 52.348 46 46 46C39.652 46 34.5 40.848 34.5 34.5C34.5 28.152 39.652 23 46 23C52.348 23 57.5 28.152 57.5 34.5Z" fill="#D2D3D4"/>
+    </svg>
+  )
+}
+
+
---
+++ src/components/icons/skills/secondary/Kotlin.tsx
@@ -0,0 +1,38 @@
+
+import { CSSProperties } from "react"
+
+export const Kotlin = (props: { style: CSSProperties | undefined }) => {
+  return (
+    <svg width="69" height="69" viewBox="0 0 69 69" fill="none" xmlns="http://www.w3.org/2000/svg" style={props.style}>
+    <g clip-path="url(#clip0_79_145)">
+    <path d="M34.6437 0L0 36.432V69L34.592 34.3447L69 0H34.6437Z" fill="url(#paint0_linear_79_145)"/>
+    <path d="M0 69L34.592 34.3477L69 69H0Z" fill="url(#paint1_linear_79_145)"/>
+    <path d="M0 0H34.6437L0 36.432V0Z" fill="url(#paint2_linear_79_145)"/>
+    </g>
+    <defs>
+    <linearGradient id="paint0_linear_79_145" x1="-11.615" y1="63.4513" x2="52.6125" y2="-0.776248" gradientUnits="userSpaceOnUse">
+    <stop offset="0.108" stop-color="#9E9E9E"/>
+    <stop offset="0.173" stop-color="#B1B1B1"/>
+    <stop offset="0.492" stop-color="#D3D3D3"/>
+    <stop offset="0.716" stop-color="white"/>
+    <stop offset="0.823" stop-color="white"/>
+    </linearGradient>
+    <linearGradient id="paint1_linear_79_145" x1="22.0225" y1="81.4488" x2="53.3025" y2="50.1688" gradientUnits="userSpaceOnUse">
+    <stop offset="0.296" stop-color="white"/>
+    <stop offset="0.694" stop-color="#CECCCB"/>
+    <stop offset="1" stop-color="#9E9E9E"/>
+    </linearGradient>
+    <linearGradient id="paint2_linear_79_145" x1="-0.805" y1="19.0612" x2="21.1025" y2="-2.90375" gradientUnits="userSpaceOnUse">
+    <stop offset="0.296" stop-color="white"/>
+    <stop offset="0.694" stop-color="#E0E0E0"/>
+    <stop offset="1" stop-color="#D9D9D9"/>
+    </linearGradient>
+    <clipPath id="clip0_79_145">
+    <rect width="69" height="69" fill="white"/>
+    </clipPath>
+    </defs>
+    </svg>
+  )
+}
+
+
